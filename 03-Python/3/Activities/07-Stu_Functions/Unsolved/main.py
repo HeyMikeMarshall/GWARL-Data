e99dc323ea806226c8796d3a7dee36b6d47a1692
HeyMikeMarshall <mjmarshall@gmail.com>
2019-03-23 13:34:53 -0400

    completed python functions unit

--- 03-Python/3/Activities/07-Stu_Functions/Unsolved/main.py
+++ 03-Python/3/Activities/07-Stu_Functions/Unsolved/main.py
@@ -4,3 +4,12 @@
 # Test your function with the following:
 # print(average([1, 5, 9]))
 # print(average(range(11)))
+
+def average(mylist):
+    if (len(mylist) == 0): return
+    currentsum = 0
+    for number in mylist:
+        currentsum += number
+    return currentsum / len(mylist)
+
+print (average([2, 5, 3, 10]))
