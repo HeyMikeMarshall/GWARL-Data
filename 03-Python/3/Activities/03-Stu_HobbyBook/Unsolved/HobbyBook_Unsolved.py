df67514db8c51ab344084df21e6e552c62df02b5
HeyMikeMarshall <mjmarshall@gmail.com>
2019-03-23 12:23:19 -0400

    class 9 - post break commit

--- 03-Python/3/Activities/03-Stu_HobbyBook/Unsolved/HobbyBook_Unsolved.py
+++ 03-Python/3/Activities/03-Stu_HobbyBook/Unsolved/HobbyBook_Unsolved.py
@@ -1,1 +1,23 @@
-# @TODO: Your code here
+
+
+me_hobbybook = {
+    "name":"Michael Marshall",
+    "age":"32",
+    "hobbies": 
+        ["hiking", "gaming", "diy"],
+    "wakeuptime": {
+        "Monday": '0600',
+        "Tuesday": '0600',
+        "Wednesday": '0600',
+        "Thursday": '0600',
+        "Friday": '0600',
+        "Saturday": '0800',
+        "Sunday": '1100',
+
+    }
+
+
+
+}
+
+print(f"My name is {me_hobbybook['name']}. One of my hobbies is {me_hobbybook['hobbies'][0]}.")
